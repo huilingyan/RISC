6e7c22ee8ebde762bf2d95e65d0cfb16fafab971
Long Li <long.li@duke.edu>
2020-03-06 13:49:28 -0500

    delete coverage

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,8 @@
 
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
-scripts/coverage_summary.sh
+#scripts/coverage_summary.sh
 ls -l /
 ls -l /coverage-out/
-cp -r build/reports/clover/html/* /coverage-out/ || exit 1
+#cp -r build/reports/clover/html/* /coverage-out/ || exit 1
 
