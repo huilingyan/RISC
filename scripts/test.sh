45e23e690f93ef3ac598cf0d801aee7089850248
Long Li <long.li@duke.edu>
2020-03-06 19:47:14 -0500

    modify test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,12 @@
 
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
-#scripts/coverage_summary.sh
+scripts/coverage_summary_server.sh
+scripts/coverage_summary_client.sh
+scripts/coverage_summary_shared.sh
 ls -l /
 ls -l /coverage-out/
-#cp -r build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
 
