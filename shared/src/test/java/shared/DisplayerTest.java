39b4df9517d02edd4b2de04812983424ecff2a28
HuilingYan <hy165@duke.edu>
2020-03-13 21:34:30 -0400

    added some testcase

---
+++ shared/src/test/java/shared/DisplayerTest.java
@@ -0,0 +1,61 @@
+package shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+public class DisplayerTest {
+  @Test
+  public void test_displayMap() {
+      Displayer displayer = Displayer.getInstance();
+      displayer.setNumOfPlayer(3);
+      Territory t0 = new Territory(0, 0, "Red");
+      Territory t1 = new Territory(0, 1, "Blue");
+      Territory t2 = new Territory(1, 2, "Green");
+      Territory t3 = new Territory(2, 3, "Yellow");
+      Territory t4 = new Territory(2, 4, "Purple");
+
+      // set number of defenders
+      t0.setDefenderNum(4);
+      t1.setDefenderNum(5);
+      t2.setDefenderNum(2);
+      t3.setDefenderNum(3);
+      t4.setDefenderNum(6);
+
+      t0.setNeighbor(0, t1);
+      t1.setNeighbor(0, t0);
+      t1.setNeighbor(1, t2);
+      t2.setNeighbor(0, t1);
+      t2.setNeighbor(1, t3);
+      t2.setNeighbor(2, t4);
+      t3.setNeighbor(0, t2);
+      t3.setNeighbor(1, t4);
+      t4.setNeighbor(0, t2);
+      t4.setNeighbor(1, t3);
+
+      ArrayList<Territory> t_map = new ArrayList<Territory>();
+      t_map.add(t0);
+      t_map.add(t1);
+      t_map.add(t2);
+      t_map.add(t3);
+      t_map.add(t4);
+
+      displayer.displayMap(t_map);
+      displayer.displayIntroduction(t_map, 2);
+  }
+
+  @Test
+  public void test_Messages() {
+    Displayer displayer = Displayer.getInstance();
+    displayer.displayBeforeInitMap();
+    displayer.connEstablishedMsg(1);
+    displayer.inputPlayerNum();
+    displayer.disConnectMsg();
+    displayer.showRemainUnitNumber(20, "Pikachu");
+    displayer.winnerAnnouncement();
+    displayer.loseGamePrompt();
+    displayer.askForExit();
+  }
+
+}
