ef147102ba049560971f89af0230b0dc6f3f5830
kh385 <kh385@duke.edu>
2020-03-18 15:41:25 -0400

    modify attack sequence with Collection.shuffle()

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -2,9 +2,11 @@ package server;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Iterator;
+import java.util.Collections;
 import shared.*;
 
 public class GameHandler extends Handler {
@@ -112,52 +114,50 @@ public class GameHandler extends Handler {
       int winner_id = defender_id;
       int winner_unit_num = defender_unit_num;
 
-      while (!t_entry.getValue().isEmpty()) {
-        //if multiple players attack the same territory,
-        //each attack is resolved sequentially
-        //first pick a random attacker, fights with current defender
-        //then pick second random attack fights with winner of first attack
-        //until all attack are handled
-        for (Iterator<Map.Entry<Integer, Integer>> it = t_entry.getValue().entrySet().iterator(); it.hasNext();) {
-          Map.Entry<Integer, Integer> p_entry = it.next();
-          //TODO: make a more reasonable shuffling
-          int rnd = (int) (Math.random() * 2);
-          if (rnd == 0) {
-            //randomize attack sequence
-            //50% to skip current attack order, making it at back of sequence 
-            continue;
+      //if multiple players attack the same territory,
+      //each attack is resolved sequentially
+      //first pick a random attacker, fights with current defender
+      //then pick second random attack fights with winner of first attack
+      //until all attack are handled
+        
+      // Get all the entries in the map into a list
+      List<Map.Entry<Integer, Integer>> entry = new ArrayList<>(t_entry.getValue().entrySet());
+ 
+      // Shuffle the list, randomize attack sequence
+      Collections.shuffle(entry);
+ 
+      // Insert them all into a LinkedHashMap
+      //Map<Integer, Integer> shuffledmap = new LinkedHashMap<>();
+      for (Map.Entry<Integer, Integer> p_entry : entry) {
+        //execute combat calculation 
+        int attacker_id = p_entry.getKey();
+        int attacker_unit_num = p_entry.getValue();
+        System.out.println("On territory:" + t_defender.getName());
+        System.out.println("player " + attacker_id + " with " + attacker_unit_num + " units" + " ATTACKS player " + defender_id + " with " + defender_unit_num + " units");
+        while ((attacker_unit_num > 0) && (defender_unit_num > 0)) {
+          //combat: roll 2 dices, one for attacker, one for defender
+          //one with lower points loses 1 unit (in a tie defender wins)
+          if (atk_dice.rollDice() > def_dice.rollDice()) {
+            defender_unit_num--;
           } else {
-            //execute combat calculation 
-            int attacker_id = p_entry.getKey();
-            int attacker_unit_num = p_entry.getValue();
-            //System.out.println("On territory:" + t_defender.getName());
-            //System.out.println("player " + attacker_id + " with " + attacker_unit_num + " units" + " ATTACKS player " + defender_id + " with " + defender_unit_num + " units");
-            while ((attacker_unit_num > 0) && (defender_unit_num > 0)) {
-              //combat: roll 2 dices, one for attacker, one for defender
-              //one with lower points loses 1 unit (in a tie defender wins)
-              if (atk_dice.rollDice() > def_dice.rollDice()) {
-                defender_unit_num--;
-              } else {
-                attacker_unit_num--;
-              }
-            }
-            if (attacker_unit_num <= 0) {
-              winner_id = defender_id;
-              winner_unit_num = defender_unit_num;
-            } else {
-              winner_id = attacker_id;
-              winner_unit_num = attacker_unit_num;
-              //attacker becomes defender for possible upcoming attacks
-              defender_id = winner_id;
-              defender_unit_num = winner_unit_num;
-
-            }
-            //System.out.println("player " + winner_id + " WINS, remaining " + winner_unit_num + " units");
-            it.remove();
+            attacker_unit_num--;
           }
         }
+        if (attacker_unit_num <= 0) {
+          winner_id = defender_id;
+          winner_unit_num = defender_unit_num;
+        } else {
+          winner_id = attacker_id;
+          winner_unit_num = attacker_unit_num;
+          //attacker becomes defender for possible upcoming attacks
+          defender_id = winner_id;
+          defender_unit_num = winner_unit_num;
 
+        }
+        System.out.println("player " + winner_id + " WINS, remaining " + winner_unit_num + " units");
+            
       }
+        
       //winner take over the territory and change ownership
       t_defender.setOwnership(winner_id);
       t_defender.setDefenderNum(winner_unit_num);
