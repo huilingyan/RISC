e9697b44685560132273b2e24aa5ac77655af3a5
HuilingYan <hy165@duke.edu>
2020-03-30 11:33:05 -0400

    map can be shown

--- client/src/main/java/client/controller/InitController.java
+++ client/src/main/java/client/controller/InitController.java
@@ -4,6 +4,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.GridPane;
@@ -11,8 +12,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.Button;
 import javafx.geometry.Insets;
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.Integer;
+import java.lang.String;
+import java.util.ArrayList;
 
 public class InitController extends SceneController {
+
+    private HashMap<Integer, String> territorylist = new HashMap<Integer, String>();
 
     @Override
     public Scene getCurrScene() {
@@ -24,31 +32,63 @@ public class InitController extends SceneController {
         root.setTop(l);
 
         // set left
-        // AnchorPane leftpane = new AnchorPane();
-        // leftpane.setPadding(new Insets(10, 10, 10, 10));
-        // root.setLeft(leftpane);
-
-        Button button1 = new Button("Mew");
-        button1.setPrefWidth(50);
-        button1.setPrefHeight(50);
-        button1.setStyle("-fx-shape: \"M 700 450 L 625 325 L 700 200 L 850 200 L 925 325 L 850 450 Z\";");
-        // AnchorPane.setRightAnchor(button1, 10.0); // this code is no effect
+        Pane leftpane = new Pane();
+        leftpane.setPadding(new Insets(10, 10, 10, 10));
+        root.setLeft(leftpane);
         
-        Button button2 = new Button("Ditto");
-        button2.setPrefWidth(25);
-        button2.setPrefHeight(25);
-        button2.setStyle("-fx-shape: \"M 700 450 L 625 325 L 700 200 L 850 200 L 925 325 L 850 450 Z\";");
-        
-        Group buttongroup = new Group();
-        buttongroup.getChildren().addAll(button1, button2);
-        root.setLeft(buttongroup);
+        Group buttongroup = generateMap();
+        leftpane.getChildren().add(buttongroup);
 
-        // leftpane.getChildren().add(buttongroup);
-    
+        root.setLeft(leftpane);
+  
         // set scene
         Scene mapscene = new Scene(root, 960, 720);
 
         return mapscene;
 
+    }
+
+    private void addTerritoryList() {
+        this.territorylist.put(0, "Pikachu");
+        this.territorylist.put(1, "Ditto");
+        this.territorylist.put(2, "Gengar");
+        this.territorylist.put(3, "Eevee");
+
+        this.territorylist.put(4, "Snorlax");
+        this.territorylist.put(5, "Mew");
+        this.territorylist.put(6, "Psyduck");
+        this.territorylist.put(7, "Magneton");
+
+        this.territorylist.put(8, "Vulpix");
+        this.territorylist.put(9, "Jumpluff");
+        this.territorylist.put(10, "Bulbasaur");
+        this.territorylist.put(11, "Charmandar");
+
+        this.territorylist.put(12, "Squirtle");
+        this.territorylist.put(13, "Pidgey");
+        this.territorylist.put(14, "Caterpie");
+        this.territorylist.put(15, "Rattata");
+    }
+
+    private Group generateMap() {
+
+        this.addTerritoryList();
+
+        Group buttongroup = new Group();
+        int init_x = 50;
+        int init_y = 50;
+
+        for (int i = 0; i < 16; i++) {
+            Button button = new Button(this.territorylist.get(i));
+            button.setPrefWidth(100);
+            button.setPrefHeight(100);
+            button.setLayoutX(init_x + 75 * (i / 4));
+            button.setLayoutY(init_y + 100 * (i % 4) + ((i % 8 > 3)? 50 : 0));
+            button.setStyle("-fx-shape: \"M 700 450 L 625 325 L 700 200 L 850 200 L 925 325 L 850 450 Z\";");
+            
+            buttongroup.getChildren().addAll(button);
+        }
+
+        return buttongroup;
     }
 }
