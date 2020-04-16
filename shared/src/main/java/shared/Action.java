843cc0dae2577e7223741316570b80ef5520c559
kh385 <kh385@duke.edu>
2020-04-15 22:21:01 -0400

    evolution 3 class modification1

--- shared/src/main/java/shared/Action.java
+++ shared/src/main/java/shared/Action.java
@@ -11,6 +11,9 @@ public class Action implements Serializable {
   private ArrayList<AttackOperation> attackOperations;
   private ArrayList<UpgradeOperation> upgradeOperations;
   private HashMap<Integer, Boolean> upgradeMaxTechLv;//pid, upgrade or not
+  private HashMap<Integer, Integer> allianceRequests;// pid1 allies pid2
+  private HashMap<Integer, Integer> newCards; //pid, cid
+  //which player uses which card
   
   //first handle upgrade, then move, finally attack
   //gui should disable UpgradeUnits after client made a Move or Attack operation
@@ -21,6 +24,8 @@ public class Action implements Serializable {
     this.attackOperations = new ArrayList<AttackOperation>();
     this.upgradeOperations = new ArrayList<UpgradeOperation>();
     this.upgradeMaxTechLv = new HashMap<Integer, Boolean>();
+    this.allianceRequests = new HashMap<Integer, Integer>();
+    this.newCards = new HashMap<Integer, Integer>();
   }
 
   public ArrayList<InitOperation> getInitOperations() {
@@ -41,6 +46,14 @@ public class Action implements Serializable {
 
   public HashMap<Integer, Boolean> getUpgradeMaxTechHashMap() {
       return upgradeMaxTechLv;
+  }
+
+  public HashMap<Integer, Integer> getAllianceRequests() {
+    return allianceRequests;
+  }
+
+  public HashMap<Integer, Integer> getNewCards(){
+    return newCards;
   }
   
   public boolean isUpgradeMaxTechLv(int pid) {
@@ -70,7 +83,16 @@ public class Action implements Serializable {
   }
 
   public void addAttackOperation(AttackOperation aop) {
-      this.attackOperations.add(aop);
+    this.attackOperations.add(aop);
+  }
+
+  public void addAllianceRequest(int pid1, int pid2) {
+    this.allianceRequests.put(pid1, pid2);
+  }
+
+  public void useNewCard(int pid, int cid) {
+    //player pid choose to use card cid this turn
+    this.newCards.put(pid, cid);
   }
 
   public void concatInitOperation(Action clientaction) {
@@ -86,11 +108,32 @@ public class Action implements Serializable {
     //if any player choose to upgrade his max tech level, set it to true
     for (HashMap.Entry<Integer, Boolean> entry :
     clientaction.getUpgradeMaxTechHashMap().entrySet()) {
-    
+
       if (entry.getValue()) {
         //replace is not suitable here because it doesn't replace null value
         upgradeMaxTechLv.put(entry.getKey(), entry.getValue());
         //System.out.println("player " + entry.getKey() + " choose to upgrade max tech lv " + upgradeMaxTechLv.get(entry.getKey()) );
+      }
+    }
+
+    //combine alliance requests
+    for (HashMap.Entry<Integer, Integer> entry :
+    clientaction.getAllianceRequests().entrySet()) {
+      if (!allianceRequests.containsKey(entry.getKey())) {
+        //if player has no alliance requests with another player
+        //add request to hashmap
+        //but if AB requested alliance, second request AC will be neglected
+        allianceRequests.put(entry.getKey(), entry.getValue());
+      }
+    }
+
+    //combine card activation
+    for (HashMap.Entry<Integer, Integer> entry :
+    clientaction.getNewCards().entrySet()) {
+      if (!newCards.containsKey(entry.getKey())) {
+        //if player didn't use any card this turn
+        //add card to hashmap
+        newCards.put(entry.getKey(), entry.getValue());
       }
     }
   }
---
+++ shared/src/main/java/shared/ChatMessage.java
@@ -0,0 +1,44 @@
+package shared;
+
+public class ChatMessage {
+
+  private int fromPid;
+  private int toPid;
+  private String message;
+
+  public ChatMessage(int from, int to, String str) {
+    fromPid = from;
+    toPid = to;
+    message = str;
+  }
+  
+  //construct a tunnel without setting message 
+  public ChatMessage(int from, int to){
+    fromPid = from;
+    toPid = to;
+  }
+
+  public int getFromPid() {
+    return fromPid;
+  }
+
+  public int getToPid() {
+    return toPid;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setFromPid(int from){
+    fromPid = from;
+  }
+
+  public void setToPid(int to) {
+    toPid = to;
+  }
+
+  public void setMessage(String str){
+    message = str;
+  }
+}
