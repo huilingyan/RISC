d41fb28d89b0b97984349a5bd48b1fa9782e23d3
jx94 <junyi.xiao@duke.edu>
2020-04-20 22:05:45 -0400

    changed communication to chat server to nio, in gameclient

--- client/src/main/java/client/GameClient.java
+++ client/src/main/java/client/GameClient.java
@@ -3,7 +3,12 @@ package client;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+import org.apache.commons.lang3.SerializationUtils;
 
 import shared.Action;
 import shared.ClientMessage;
@@ -12,25 +17,26 @@ import shared.ChatMessage;
 
 public class GameClient {
 
-    
     Socket serverSocket; // set up when connect
     ObjectInputStream inStream; // set up right before first recv
     ObjectOutputStream outStream; // set up when connect
     // The socket and stream for chatserver
-    Socket chatSocket; // socket for chatserver
-    ObjectOutputStream chatOutStream; // stream for chatserver
+    // Socket chatSocket; // socket for chatserver
+    // ObjectOutputStream chatOutStream; // stream for chatserver
+    SocketChannel chatChannel;
+
     /****
-    Connect to the server host provided in config file
-    ***/
+     * Connect to the server host provided in config file
+     ***/
     public void connectToServer() {
-        Config config = new Config("config.properties"); 
-        String host = config.readProperty("hostname"); 
+        Config config = new Config("config.properties");
+        String host = config.readProperty("hostname");
         String port = config.readProperty("port");
         try {
-            Socket newSocket = new Socket(host, Integer.parseInt(port)); 
+            Socket newSocket = new Socket(host, Integer.parseInt(port));
             this.serverSocket = newSocket;
         } catch (IOException e) {
-            System.out.println("Cannot connect to server at " + host + ": " + port); 
+            System.out.println("Cannot connect to server at " + host + ": " + port);
         }
         // open outputstream
         try {
@@ -38,43 +44,64 @@ public class GameClient {
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("fail to set up ObjectOutputStream");
-        } 
+        }
     }
+
     /****
-    Connect to the ChatServer
-    ***/
+     * Connect to the ChatServer
+     ***/
+    // public void connectToChatServer() {
+    // Config config = new Config("config.properties");
+    // String host = config.readProperty("hostname");
+    // String chat_port = config.readProperty("chat_port");
+    // try {
+    // Socket newChatSocket = new Socket(host, Integer.parseInt(chat_port));
+    // this.chatSocket = newChatSocket;
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // System.out.println("Cannot connect to chatServer at " + host + ": " +
+    // chat_port);
+    // }
+    // // open outputstream
+    // try {
+    // this.chatOutStream = new
+    // ObjectOutputStream(this.chatSocket.getOutputStream());
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // System.out.println("fail to set up ObjectOutputStream");
+    // }
+    // }
+
     public void connectToChatServer() {
-        Config config = new Config("config.properties"); 
-        String host = config.readProperty("hostname"); 
+        Config config = new Config("config.properties");
+        String host = config.readProperty("hostname");
         String chat_port = config.readProperty("chat_port");
         try {
-            Socket newChatSocket = new Socket(host, Integer.parseInt(chat_port));
-            this.chatSocket = newChatSocket; 
+            this.chatChannel = SocketChannel.open();
+            this.chatChannel.connect(new InetSocketAddress(host, Integer.parseInt(chat_port)));
         } catch (IOException e) {
+            System.out.println("IOException when connects to chat server");
             e.printStackTrace();
-            System.out.println("Cannot connect to chatServer at " + host + ": " + chat_port); 
+            System.exit(0);
         }
-        // open outputstream
-        try {
-            this.chatOutStream = new ObjectOutputStream(this.chatSocket.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("fail to set up ObjectOutputStream");
-        } 
+
     }
-    /***** Initialize an inputstream from the socket. Call it right before
-    receiving RoomMessage from server
-    ****/
+
+    /*****
+     * Initialize an inputstream from the socket. Call it right before receiving
+     * RoomMessage from server
+     ****/
     public void setUpInputStream() {
         try {
-            this.inStream = new ObjectInputStream(this.serverSocket.getInputStream()); 
+            this.inStream = new ObjectInputStream(this.serverSocket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
-        }        
+        }
     }
+
     /****
-    Recv an object from server, return null on error
-    ****/
+     * Recv an object from server, return null on error
+     ****/
     public Object recvObject() {
         try {
             return this.inStream.readObject();
@@ -87,46 +114,114 @@ public class GameClient {
         }
         return null;
     }
-        
+
     /****
-    Send an object to server
-    *****/
+     * Send an object to server
+     *****/
     public void sendObject(Object ob) {
         try {
-            this.outStream.writeObject(ob); 
+            this.outStream.writeObject(ob);
         } catch (IOException e) {
             closeSocket();
             System.exit(0); // exit program if server's down }
         }
     }
+
     /****
-    Send chatMessage to chatServer
-    *****/
+     * Send chatMessage to chatServer
+     *****/
+    // public void sendChatMsg(ChatMessage chatMsg) {
+    // try {
+    // this.chatOutStream.writeObject(chatMsg);
+    // } catch (IOException e) {
+    // closeSocket();
+    // System.exit(0); // exit program if server's down }
+    // }
+    // }
+
     public void sendChatMsg(ChatMessage chatMsg) {
+        byte[] chatBytes = SerializationUtils.serialize(chatMsg);
+        ByteBuffer writeBuffer = ByteBuffer.wrap(chatBytes);
         try {
-            this.chatOutStream.writeObject(chatMsg); 
+            this.chatChannel.write(writeBuffer);
         } catch (IOException e) {
-            closeSocket();
-            System.exit(0); // exit program if server's down }
+            System.out.println("IOException when send message to chat server");
+            e.printStackTrace();
+            System.exit(0);
         }
+        writeBuffer.clear();
     }
+
+    /**
+     * Only return a ChatMessage when receives one
+     * @return
+     */
+    public ChatMessage recvChatMsg() {
+        ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+        while (true) {
+            int readBytes = 0;
+            try {
+                readBytes = this.chatChannel.read(readBuffer);
+            } catch (IOException e) {
+                System.out.println("IOException when recv message from chat server");
+                e.printStackTrace();
+                System.exit(0);
+            }
+            if (readBytes == 0) {
+                continue;  // no message, continue
+            }
+            // while (readBytes > 0) {
+            // // debug
+            // System.out.println("received message, length " + readBytes);
+            // readBuffer.flip();
+            // while (readBuffer.hasRemaining()) {
+            // // System.out.print((char) readBuffer.get());
+            // System.out.println("has remaining");
+            // readBuffer.get();
+            // }
+            // System.out.println("out of has remaining loop");
+            // readBuffer.clear();
+            // System.out.println("clear read buffer");
+            // readBytes = this.chatChannel.read(readBuffer);
+            // System.out.println(readBytes);
+            // }
+            if (readBytes == -1) {
+                // this.chatChannel.close();
+                // System.out.println("close channel");
+                System.out.println("readBytes = -1");
+                System.exit(0);
+            }
+            // System.out.println(readBuffer.array().length);
+            ChatMessage chatMsgRecv = (ChatMessage) SerializationUtils.deserialize(readBuffer.array());
+            readBuffer.clear();
+            // String recv = new String(readBuffer.array()).trim();
+            // debug
+            System.out.println("The chat message is from: " + chatMsgRecv.getSrcPlayerName());
+            System.out.println("To: " + chatMsgRecv.getDestPlayerName());
+            System.out.println("Saying: " + chatMsgRecv.getMessage());
+            return chatMsgRecv;
+        }
+
+    }
+
     /***
-     Send switch out message to server
-    ***/
+     * Send switch out message to server
+     ***/
     public void sendSwitchOutMsg() {
-        sendObject(new ClientMessage(0, 3, new Action())); 
+        sendObject(new ClientMessage(0, 3, new Action()));
     }
+
     /****
-    Close the socket and any open output/input stream
-    ****/
+     * Close the socket and any open output/input stream
+     ****/
     public void closeSocket() {
         try {
-            chatOutStream.close(); 
+            // chatOutStream.close();
             outStream.close();
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Failed to close");
-        } 
+        }
     }
-    
+
 }
--- server/src/main/java/server/ChatServer.java
+++ server/src/main/java/server/ChatServer.java
@@ -12,11 +12,13 @@ import java.nio.channels.SelectionKey;
 import java.util.Set;
 import java.util.Iterator;
 import java.lang.String;
+import java.math.BigInteger;
 import java.lang.Integer;
-import java.util.HashMap; 
+import java.util.HashMap;
 import java.util.Map;
 import java.net.Socket;
 import shared.*;
+import java.nio.charset.StandardCharsets;
 
 /****
  * A class that implements the chat between players(server side)
@@ -28,21 +30,21 @@ public class ChatServer extends Thread {
     private ServerSocketChannel ssc = null;
     private Selector selector = null;
     private Gameserver boss;
-    private HashMap<String, SocketChannel> socketMap = new HashMap<> ();
+    private HashMap<String, SocketChannel> socketMap = new HashMap<>();
 
     // constructor
     public ChatServer(Gameserver gServer) {
         this.boss = gServer;
     }
- 
+
     @Override
     public void run() {
         try {
             this.init();
             this.process();
 
-        } catch (IOException e) {    
-            e.printStackTrace();  
+        } catch (IOException e) {
+            e.printStackTrace();
 
         } finally {
             try {
@@ -58,7 +60,7 @@ public class ChatServer extends Thread {
         }
     }
 
-    public void init() throws IOException {  
+    public void init() throws IOException {
 
         this.ssc = ServerSocketChannel.open(); // open channel
         Config config = new Config("config.properties");
@@ -70,13 +72,15 @@ public class ChatServer extends Thread {
         this.ssc.register(selector, SelectionKey.OP_ACCEPT); // register channel with the selector
     }
 
-    public void process() throws IOException {   
+    public void process() throws IOException {
 
-        while (true) {           
+        while (true) {
+            // debug
+            // System.out.println("Start a loop");
             int nRead = this.selector.selectNow();
             if (nRead == 0) {
                 continue;
-            }       
+            }
             Set<SelectionKey> selectedKeys = this.selector.selectedKeys();
             Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
 
@@ -88,28 +92,36 @@ public class ChatServer extends Thread {
                         SocketChannel clientChannel = this.ssc.accept();
                         clientChannel.configureBlocking(false);
                         clientChannel.register(selector, SelectionKey.OP_READ);
-                        //debug
+                        // debug
                         System.out.println("Accepted a connection!");
-    
+
                     } else if (key.isReadable()) {
-                        SocketChannel clientChannel = (SocketChannel)key.channel();
+                        SocketChannel clientChannel = (SocketChannel) key.channel();
                         ByteBuffer readBuffer = ByteBuffer.allocate(1024);
                         int readBytes = clientChannel.read(readBuffer);
-                        
-                        while (readBytes > 0) {
-                            readBuffer.flip();
-                            while (readBuffer.hasRemaining()) {
-                                // System.out.print((char) readBuffer.get());
-                                readBuffer.get();
-                            }                                                          
-                            readBuffer.clear();
-                            readBytes = clientChannel.read(readBuffer);
-                        }
+
+                        // while (readBytes > 0) {
+                        //     readBuffer.flip();
+                        //     while (readBuffer.hasRemaining()) {
+                        //         // System.out.print((char) readBuffer.get());
+                        //         readBuffer.get();
+                        //     }
+                        //     readBuffer.clear();
+                        //     readBytes = clientChannel.read(readBuffer);
+                        // }
                         if (readBytes == -1) {
                             // key.cancel();
                             clientChannel.close();
                         }
-                        ChatMessage chatMsgRecv = (ChatMessage)SerializationUtils.deserialize(readBuffer.array());
+                        // debug
+                        // String str = new String(readBuffer.array(), StandardCharsets.UTF_8);
+                        // System.out.println(new String(chatBytes, StandardCharsets.UTF_8));
+                        // String newStr = String.format("%040x", new BigInteger(1, str.getBytes(StandardCharsets.UTF_8)));
+                        // Hex.encodeHexString(str.getBytes(StandardCharsets.UTF_8));
+                        // System.out.println(newStr);
+                        // System.out.println(new String(readBuffer.array(), StandardCharsets.UTF_8));
+                        ChatMessage chatMsgRecv = (ChatMessage) SerializationUtils.deserialize(readBuffer.array());
+                        readBuffer.clear();
                         // String recv = new String(readBuffer.array()).trim();
                         // debug
                         System.out.println("The chat message is from: " + chatMsgRecv.getSrcPlayerName());
@@ -118,13 +130,14 @@ public class ChatServer extends Thread {
                         // System.out.println("Server received: " + recv);
                         HandleChatMsg(clientChannel, chatMsgRecv);
                         key.cancel();
-    
-                    }
-                    else {
+
+                    } else {
                         System.out.println("Wrong type of channel!"); // if received connectable channel
                     }
-                }                
+                }
                 keyIterator.remove(); // remove from sets as long as no data to process
+                // debug
+
             }
         }
     }
@@ -134,9 +147,9 @@ public class ChatServer extends Thread {
      */
     private void HandleChatMsg(SocketChannel clientChannel, ChatMessage chatMsgRecv) throws IOException {
         // String msgString = "user message stored";
-        // byte [] msg = msgString.getBytes();  
-        // ByteBuffer writebuffer = ByteBuffer.wrap(msg);  
-        // clientChannel.write(writebuffer); 
+        // byte [] msg = msgString.getBytes();
+        // ByteBuffer writebuffer = ByteBuffer.wrap(msg);
+        // clientChannel.write(writebuffer);
         // //debug
         // System.out.println("Confirm message sent");
 
@@ -145,21 +158,18 @@ public class ChatServer extends Thread {
             this.socketMap.put(chatMsgRecv.getSrcPlayerName(), clientChannel);
             // send confirm msg back
             sendConfirmMsg(clientChannel, chatMsgRecv.getSrcPlayerName());
-            
-        }
-        else { // process normal chat msg from players
+
+        } else { // process normal chat msg from players
             SocketChannel playerToSend = findChannelByPlayername(destPlayerName);
             if (playerToSend == null) { // if no such player
                 noSuchPlayer(clientChannel, chatMsgRecv.getSrcPlayerName());
 
-            }
-            else {
+            } else {
                 if (isActiveGame(chatMsgRecv)) { // if dest player is in the same game
                     ByteBuffer writeBuffer = ByteBuffer.wrap(SerializationUtils.serialize(chatMsgRecv));
                     playerToSend.write(writeBuffer);
                     writeBuffer.clear();
-                }
-                else { // if dest player is currently out of the game
+                } else { // if dest player is currently out of the game
                     inactiveGame(clientChannel, chatMsgRecv.getSrcPlayerName());
                 }
             }
@@ -186,37 +196,41 @@ public class ChatServer extends Thread {
     }
 
     /*
-      Confirm msg sent back to src player
-    */
+     * Confirm msg sent back to src player
+     */
     private void sendConfirmMsg(SocketChannel clientChannel, String srcPlayerName) throws IOException {
         ChatMessage confirmMsg = new ChatMessage("ChatServer", srcPlayerName, "User message received");
         ByteBuffer writeBuffer = ByteBuffer.wrap(SerializationUtils.serialize(confirmMsg));
-        clientChannel.write(writeBuffer);
-        writeBuffer.clear();        
-    }
-    /*
-      Error msg sent back to src player that the dest player does not exist
-    */
-    private void noSuchPlayer(SocketChannel clientChannel, String srcPlayerName) throws IOException {
-        ChatMessage noSuchPlayerMsg = new ChatMessage("ChatServer", srcPlayerName, "No such player exists!");
-        ByteBuffer writeBuffer = ByteBuffer.wrap(SerializationUtils.serialize(noSuchPlayerMsg));
         clientChannel.write(writeBuffer);
         writeBuffer.clear();
     }
 
     /*
-      Warning msg sent back to src player that the dest player is offline
-    */
+     * Error msg sent back to src player that the dest player does not exist
+     */
+    private void noSuchPlayer(SocketChannel clientChannel, String srcPlayerName) throws IOException {
+        ChatMessage noSuchPlayerMsg = new ChatMessage("ChatServer", srcPlayerName, "No such player exists!");
+        ByteBuffer writeBuffer = ByteBuffer.wrap(SerializationUtils.serialize(noSuchPlayerMsg));
+        clientChannel.write(writeBuffer);
+        // // debug
+        // System.out.println("Sent no such player message to client " + srcPlayerName);
+        writeBuffer.clear();
+    }
+
+    /*
+     * Warning msg sent back to src player that the dest player is offline
+     */
     private void inactiveGame(SocketChannel clientChannel, String srcPlayerName) throws IOException {
-        ChatMessage outOfGameMsg = new ChatMessage("ChatServer", srcPlayerName, "The player you're looking for is currently offline");
+        ChatMessage outOfGameMsg = new ChatMessage("ChatServer", srcPlayerName,
+                "The player you're looking for is currently offline");
         ByteBuffer writeBuffer = ByteBuffer.wrap(SerializationUtils.serialize(outOfGameMsg));
         clientChannel.write(writeBuffer);
         writeBuffer.clear();
     }
 
     // public static void main(String[] args) {
-    //     // run the game
-    //     ChatServer charServer = new ChatServer();
-    //     charServer.run();
-    //   }
+    // // run the game
+    // ChatServer charServer = new ChatServer();
+    // charServer.run();
+    // }
 }
