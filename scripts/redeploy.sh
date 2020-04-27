b5f6efe4958053a6be662bb061ab188c00466a36
jx94@duke.edu <jx94@duke.edu>
2020-04-27 15:04:09 -0400

    try run postgres in shell executor

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,7 +5,19 @@ docker kill citest-651
 docker rm citest-651
 
 # now run the new one
-# docker run -d --name citest-651 -p 1651:7777 6666:6666 -t citest ./gradlew run-server
-apt-get install docker-compose
-docker-compose up
+# set up postgres
+sudo apt-get install -y postgresql postgresql-client libpq-dev
+sudo -u postgres psql -d template1
+template1=# CREATE USER runner WITH PASSWORD 'abc123' CREATEDB;
+template1=# CREATE DATABASE risc OWNER runner;
+template1=# \q
+psql -U runner -h localhost -d risc -W
+
+# old code
+docker run -d --name citest-651 -p 1651:7777 6666:6666 -t citest ./gradlew run-server
+
+
+# docker compose
+# sudo apt-get install -y docker-compose
+# docker-compose up
 
