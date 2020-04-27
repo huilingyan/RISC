e3337bc0e1d4039ba7f34c9938ab0ee95eb5ab62
jx94@duke.edu <jx94@duke.edu>
2020-04-27 14:53:10 -0400

    added docker-compose

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -5,5 +5,7 @@ docker kill citest-651
 docker rm citest-651
 
 # now run the new one
-docker run -d --name citest-651 -p 1651:7777 -t citest ./gradlew run-server
+# docker run -d --name citest-651 -p 1651:7777 6666:6666 -t citest ./gradlew run-server
+apt-get install docker-compose
+docker-compose up
 
