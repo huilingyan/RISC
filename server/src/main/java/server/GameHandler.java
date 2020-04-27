54ebc0598cba6aa7e2c9d375ba311702d0274a86
kh385 <kh385@duke.edu>
2020-04-27 16:43:18 -0400

    Update GameHandler.java
    use: (ownerstatus(t_dest, new_worldmap.getPlayerStatByPid(playerid)) == 1) to tell if dest is ally's territory

--- server/src/main/java/server/GameHandler.java
+++ server/src/main/java/server/GameHandler.java
@@ -140,8 +140,7 @@ public class GameHandler extends Handler {
         playerid = t_src.getOwnership();
         t_src.subtractDefender(army_move);//src territory - unit
         if(new_worldmap.getPlayerStatByPid(playerid).isAllied() &&
-           (new_worldmap.getPlayerStatByPid(t_dest.getOwnership()).getAid() ==
-            new_worldmap.getPlayerStatByPid(playerid).getAid())){
+        (ownerstatus(t_dest, new_worldmap.getPlayerStatByPid(playerid)) == 1)) {
           t_dest.addFriendDefender(army_move);//move to ally's territory
       
         } else {
@@ -514,9 +513,8 @@ public class GameHandler extends Handler {
       if (t_src != null && t_dest != null) {
         playerid = t_src.getOwnership();
         //player attacks ally's territory
-        if (new_worldmap.getPlayerStatByPid(playerid).isAllied()
-            && (new_worldmap.getPlayerStatByPid(t_dest.getOwnership()).getAid() == new_worldmap
-                .getPlayerStatByPid(playerid).getAid())) {
+        if (new_worldmap.getPlayerStatByPid(playerid).isAllied() &&
+        (ownerstatus(t_dest, new_worldmap.getPlayerStatByPid(playerid)) == 1)) {
           //change aid and allied status for both players
           int allyid = t_dest.getOwnership();
           new_worldmap.breakAlliance(playerid, allyid);
