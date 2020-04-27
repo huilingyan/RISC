a9f9554d3f91dfb792b88581bd0b761136469b33
jx94@duke.edu <jx94@duke.edu>
2020-04-27 15:14:00 -0400

    sudo apt get install docker-compose

--- scripts/redeploy.sh
+++ scripts/redeploy.sh
@@ -6,18 +6,18 @@ docker rm citest-651
 
 # now run the new one
 # set up postgres
-sudo apt-get install -y postgresql postgresql-client libpq-dev
-sudo -u postgres psql -d template1
-template1=# CREATE USER runner WITH PASSWORD 'abc123' CREATEDB;
-template1=# CREATE DATABASE risc OWNER runner;
-template1=# \q
-psql -U runner -h localhost -d risc -W
+# sudo apt-get install -y postgresql postgresql-client libpq-dev
+# sudo -u postgres psql -d template1
+# template1=# CREATE USER runner WITH PASSWORD 'abc123' CREATEDB;
+# template1=# CREATE DATABASE risc OWNER runner;
+# template1=# \q
+# psql -U runner -h localhost -d risc -W
 
-# old code
-docker run -d --name citest-651 -p 1651:7777 6666:6666 -t citest ./gradlew run-server
+# # old code
+# docker run -d --name citest-651 -p 1651:7777 6666:6666 -t citest ./gradlew run-server
 
 
 # docker compose
-# sudo apt-get install -y docker-compose
-# docker-compose up
+sudo apt-get install -y docker-compose
+sudo docker-compose up
 
